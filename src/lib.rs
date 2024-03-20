466b99fd699e206c3e2ae1603727157f12209c53
Jelle Willem Hoek <jellewhoek@gmail.com>
2024-03-20 01:15:55 +0100

    Added case sensitivity

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,28 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results: Vec<&str> = Vec::new();
     
     for line in contents.lines() {
-        if line.contains(query) {
+        if line.contains(&query) {
+            results.push(line);
+        }
+    }
+
+    return results
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
@@ -16,7 +33,13 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents: String = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}")
     }
 
@@ -26,6 +49,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -37,6 +61,12 @@ impl Config {
         let query: String = args[1].clone();
         let file_path: String= args[2].clone();
 
-        Ok(Config {query, file_path})
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
